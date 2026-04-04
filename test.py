8c8797ff4507c204261d9cf1d0acc376667da15d
kwang <kwangs7243@gmil.com>
2026-04-04 22:29:57 +0900

    ver1

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+import todo as td
+def test_add_todo():
+    t = td.todo()
+    t.add_todo("test1")
+    t.add_todo("test2")
+    data = t.get_data()
+    print(len(data) == 2)
+    print(data[0]["content"] == "test1")
+    print(data[1]["content"] == "test2")
+def test_category():
+    t = td.todo()
+    t.add_todo("test1")
+    t.add_todo("test2")
+    t.set_category("active")
+    category_data = t.category_data()
+    print(len(category_data) == 2)
+    print(category_data[0]["content"] == "test1")
+    print(category_data[1]["content"] == "test2")
+    print(category_data[0]["category"] == "active")
+    print(category_data[1]["category"] == "active") 
 
+test_add_todo()
+test_category()
--- todo.py
+++ todo.py
@@ -14,7 +14,8 @@ class todo:
         info= {
             "date" : date,
             "content" : content,
-            "completed" : False,
+            "category" : "active",
+            "deleted" : False,
         }
         self.todos.append(info)
         return
@@ -27,31 +28,9 @@ class todo:
             new_dic["original_idx"] = idx
             data.append(new_dic)
         return data
-    def category_data(self):
-        data = self.get_data()
-        if not data:
-            return
-        category = self.state["category"]
-        if category != "all":
-            category_data = [dic for dic in data if dic["category"] ==category]
-        else:
-            category_data = data
-        return category_data
     def set_category(self,category):
         self.state["category"] = category
         return
-    def sorted_data(self):
-        data = self.get_data()
-        column = self.state["sort_column"]
-        direction = self.state["sort_direction"]
-        if not data:
-            return
-        if column is not None:
-            if direction == "asc":
-                sorted_data = sorted(data,key=lambda x:x[column],reverse=False)
-            else:
-                sorted_data = sorted(data,key=lambda x:x[column],reverse=True)
-        return sorted_data
     def set_sort(self,column):
         if column != self.state["sort_column"]:
             self.state["sort_column"] = column
@@ -60,23 +39,67 @@ class todo:
         if self.state["sort_direction"] == "asc":
             self.state["sort_direction"] = "desc"
             return
-    def keyword_data(self):
-        data = self.get_data()
-        if not data:
-            return
-        keyword = self.state["keyword"]
-        keyword_data = [dic for dic in data if keyword in dic["content"]]
-        return keyword_data
     def set_keyword(self,keyword):
         keyword = keyword.strip()
         self.state["keyword"] = keyword
         return
-    def delete_data(self,idx):
-        pass
-    def set_completed(self,idx):
-        pass
-    def update_data(self,idx):
-        pass
+    def category_data(self):
+        category_data = self.get_data()
+        if not category_data:
+            return  
+        category = self.state["category"]
+        if category != "all":
+            category_data = [dic for dic in category_data if dic["category"] == category]
+        return category_data
+    def sorted_data(self):
+        sorted_data = self.get_data()
+        if not sorted_data:
+            return
+        column = self.state["sort_column"]
+        direction = self.state["sort_direction"]
+        if column is not None:
+            if direction == "asc":
+                sorted_data = sorted(sorted_data,key=lambda x:x[column],reverse=False)
+            else:
+                sorted_data = sorted(sorted_data,key=lambda x:x[column],reverse=True)
+        return sorted_data
+    def keyword_data(self):
+        keyword_data = self.get_data()
+        if not keyword_data:
+            return
+        keyword = self.state["keyword"]
+        keyword_data = [dic for dic in keyword_data if keyword in dic["content"]]
+        return keyword_data
+    def delete_data(self,number):
+        try:
+            original_idx = int(number) - 1
+        except:
+            return
+        self.todos[original_idx]["deleted"] == True
+        return
+    def set_completed(self,number):
+        try:
+            original_idx = int(number) - 1
+        except:
+            return
+        self.todos[original_idx]["completed"] = True
+        return
+    def update_data(self,number,content):
+        content = content.strip()
+        if not content:
+            return
+        try:
+            original_idx = int(number) - 1
+        except:
+            return
+        self.todos[original_idx]["content"] = content
+        return
     def view_data(self):
-        pass
+        view_data = self.get_data()
+        view_data = self.keyword_data(view_data)
+        if not view_data:
+            return
+        view_data = self.category_data(view_data)
+        view_data = self.sorted_data(view_data)
+        return view_data
 
